b5db2e45b7608996fc1b03b2a4e573fca35c84cb
Tim Born <timborn@deltacfax.com>
2017-11-06 17:28:24 -0600

    we do NOT need daemons when using Docker, so I removed them from ubuntu scripts

--- amps-1/install-amps-daemon-ubuntu.sh
+++ amps-1/install-amps-daemon-ubuntu.sh
@@ -7,7 +7,8 @@ AMPS=AMPS-5.2.0.70-Release-Linux
 # Ensure we have sbin
 PATH=/sbin:$PATH
 
-echo "Using Debian-style daemon installation."
+# NO DAEMON
+# echo "Using Debian-style daemon installation."
 
 mkdir -p $INSTALLPATH
 mkdir -p $RUNPATH
@@ -19,15 +20,15 @@ cp -r $AMPS/lib $INSTALLPATH/.
 
 echo "Installed AMPS binaries to $INSTALLPATH"
 
-echo "Copying init script to /etc/init.d/amps"
-cp -f $AMPS/bin/amps-init-script-debian /etc/init.d/amps
-ERROR=$?
-if [ ! -x /etc/init.d/amps ]; then
-   echo "Installation failed: $ERROR."
-   exit $ERROR
-fi
-echo "AMPS initialization script installed to /etc/init.d/amps"
-update-rc.d amps defaults || echo "Couldn't update!"
-echo "Loaded AMPS service definition"
+# echo "Copying init script to /etc/init.d/amps"
+# cp -f $AMPS/bin/amps-init-script-debian /etc/init.d/amps
+# ERROR=$?
+# if [ ! -x /etc/init.d/amps ]; then
+#    echo "Installation failed: $ERROR."
+#    exit $ERROR
+# fi
+# echo "AMPS initialization script installed to /etc/init.d/amps"
+# update-rc.d amps defaults || echo "Couldn't update!"
+# echo "Loaded AMPS service definition"
 
-echo "Daemon is installed. You must provide a config file at /opt/etc/amps/config.xml"
+# echo "Daemon is installed. You must provide a config file at /opt/etc/amps/config.xml"
